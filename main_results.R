a7a51d72f31f9c27f7e7f65c6e7ad57050437afe
Muhammad Fawdy Renardi Wahyu <87023372+fawdywahyu18@users.noreply.github.com>
2024-12-23 08:28:30 +0700

    Update main_results.R

--- main_results.R
+++ main_results.R
@@ -93,95 +93,18 @@ housing_tw = plm(housing_diff ~ curah_hujan_diff + temperature_diff + share_pert
                 effect = 'twoway')
 housing_tw_se = coef_test(housing_tw, vcov = "CR1", cluster = data_panel_trend$kode_bps, test = "naive-t")$SE
 
-clothing_ind = plm(clothing_diff ~ curah_hujan_diff + temperature_diff + share_pertanian + log_pdrb + share_industri + deflator_pertanian_growth + deflator_industri_growth + inflasi_lag1,
-                  data=data_panel_trend,
-                  index=c('kode_bps', 'tahun'),
-                  model='within',
-                  effect = 'individual')
-clothing_ind_se = coef_test(clothing_ind, vcov = "CR1", cluster = data_panel_trend$kode_bps, test = "naive-t")$SE
-
-clothing_tw = plm(clothing_diff ~ curah_hujan_diff + temperature_diff + share_pertanian + log_pdrb + share_industri + deflator_pertanian_growth + deflator_industri_growth + inflasi_lag1,
-                  data=data_panel_trend,
-                  index=c('kode_bps', 'tahun'),
-                  model='within',
-                  effect = 'twoway')
-clothing_tw_se = coef_test(clothing_tw, vcov = "CR1", cluster = data_panel_trend$kode_bps, test = "naive-t")$SE
-
-health_ind = plm(health_diff ~ curah_hujan_diff + temperature_diff + share_pertanian + log_pdrb + share_industri + deflator_pertanian_growth + deflator_industri_growth + inflasi_lag1,
-                 data=data_panel_trend,
-                   index=c('kode_bps', 'tahun'),
-                   model='within',
-                   effect = 'individual')
-health_ind_se = coef_test(health_ind, vcov = "CR1", cluster = data_panel_trend$kode_bps, test = "naive-t")$SE
-
-health_tw = plm(health_diff ~ curah_hujan_diff + temperature_diff + share_pertanian + log_pdrb + share_industri + deflator_pertanian_growth + deflator_industri_growth + inflasi_lag1,
-                  data=data_panel_trend,
-                  index=c('kode_bps', 'tahun'),
-                  model='within',
-                  effect = 'twoway')
-health_tw_se = coef_test(health_tw, vcov = "CR1", cluster = data_panel_trend$kode_bps, test = "naive-t")$SE
-
-#Education
-education_recreation_sport_ind = plm(education_recreation_sport_diff ~ curah_hujan_diff + temperature_diff + share_pertanian + log_pdrb + share_industri + deflator_pertanian_growth + deflator_industri_growth + inflasi_lag1,
-                                          data=data_panel_trend,
-                                          index=c('kode_bps', 'tahun'),
-                                          model='within',
-                                          effect = 'individual')
-education_recreation_sport_ind_se = coef_test(education_recreation_sport_ind, vcov = "CR1", cluster = data_panel_trend$kode_bps, test = "naive-t")$SE
-
-education_recreation_sport_tw = plm(education_recreation_sport_diff ~ curah_hujan_diff + temperature_diff + share_pertanian + log_pdrb + share_industri + deflator_pertanian_growth + deflator_industri_growth + inflasi_lag1,
-                                         data=data_panel_trend,
-                                         index=c('kode_bps', 'tahun'),
-                                         model='within',
-                                         effect = 'twoway')
-education_recreation_sport_tw_se = coef_test(education_recreation_sport_tw, vcov = "CR1", cluster = data_panel_trend$kode_bps, test = "naive-t")$SE
-
-#Transportation
-transportation_communication_finance_ind = plm(transportation_communication_finance_diff ~ curah_hujan_diff + temperature_diff + share_pertanian + log_pdrb + share_industri + deflator_pertanian_growth + deflator_industri_growth + inflasi_lag1,
-                                               data=data_panel_trend,
-                                               index=c('kode_bps', 'tahun'),
-                                               model='within',
-                                               effect = 'individual')
-transportation_communication_finance_ind_se = coef_test(transportation_communication_finance_ind, vcov = "CR1", cluster = data_panel_trend$kode_bps, test = "naive-t")$SE
-
-transportation_communication_finance_tw = plm(transportation_communication_finance_diff ~ curah_hujan_diff + temperature_diff + share_pertanian + log_pdrb + share_industri + deflator_pertanian_growth + deflator_industri_growth + inflasi_lag1,
-                                              data=data_panel_trend,
-                                              index=c('kode_bps', 'tahun'),
-                                              model='within',
-                                              effect = 'twoway')
-transportation_communication_finance_tw_se = coef_test(transportation_communication_finance_tw, vcov = "CR1", cluster = data_panel_trend$kode_bps, test = "naive-t")$SE
-
-
 
 library(stargazer)
 stargazer(composite_ind, composite_tw, food_ind, food_tw, 
           se=list(composite_ind_se, composite_tw_se, food_ind_se, food_tw_se), 
           type = 'html',
-          out = 'Laporan/Individual and Twoway/Tabel Regresi Fixed Effect Part 1 tex.html', no.space = TRUE, align = TRUE,
+          out = 'Tabel Regresi Individual and Twoway Fixed Effect Part 1 tex.html', no.space = TRUE, align = TRUE,
           omit.stat=c("LL","ser","f"))
 
 stargazer(processed_food_ind, processed_food_tw, housing_ind, housing_tw,
           se=list(processed_food_ind_se, processed_food_tw_se, housing_ind_se, housing_tw_se), 
           type = 'html',
-          out = 'Laporan/Individual and Twoway/Tabel Regresi Fixed Effect Part 2 tex.html', no.space = TRUE, align = TRUE,
-          omit.stat=c("LL","ser","f"))
-
-stargazer(clothing_ind, clothing_tw, health_ind, health_tw,
-          se=list(clothing_ind_se, clothing_tw_se, health_ind_se, health_tw_se), 
-          type = 'html',
-          out = 'Laporan/Individual and Twoway/Tabel Regresi Fixed Effect Part 3 tex.html', no.space = TRUE, align = TRUE,
-          omit.stat=c("LL","ser","f"))
-
-stargazer(education_recreation_sport_ind, education_recreation_sport_tw,
-          se=list(education_recreation_sport_ind_se, education_recreation_sport_tw_se), 
-          type = 'html',
-          out = 'Laporan/Individual and Twoway/Tabel Regresi Fixed Effect Part 4 education tex.html', no.space = TRUE, align = TRUE,
-          omit.stat=c("LL","ser","f"))
-
-stargazer(transportation_communication_finance_ind, transportation_communication_finance_tw,
-          se=list(transportation_communication_finance_ind_se, transportation_communication_finance_tw_se), 
-          type = 'html',
-          out = 'Laporan/Individual and Twoway/Tabel Regresi Fixed Effect Part 4 transportation tex.html', no.space = TRUE, align = TRUE,
+          out = 'Tabel Regresi Individual and Twoway Fixed Effect Part 2 tex.html', no.space = TRUE, align = TRUE,
           omit.stat=c("LL","ser","f"))
 
 
@@ -193,7 +116,7 @@ pdata_trend = pdata.frame(data_panel_trend, index = c("kab_kota", "tahun"), drop
 pdata_trend_selected = pdata_trend[, ]
 
 pdata_sum = xtsum(pdata_trend, variables = c('composite_diff', 'food_diff', 'processed_food_diff', 'housing_diff',
-                                             'clothing_diff', 'curah_hujan_diff', 
+                                             'curah_hujan_diff', 
                                              'temperature_diff', 'share_pertanian', 'log_pdrb', 'share_industri', 
                                              'deflator_pertanian_growth', 'deflator_industri_growth', 'inflasi_lag1'),
                   return.data.frame = TRUE)
